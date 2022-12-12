ed68ca76861223420dfe023004aa48b782600142
Yuriko <mangindev@gmail.com>
2022-12-11 20:12:12 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero-image': "url('/src/components/assets/space.jpeg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
